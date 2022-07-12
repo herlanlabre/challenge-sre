8d5b36d577e9a97a7347df9f6f414d719e776c4d
unknown <herlanroberto@gmail.com>
2022-07-12 15:21:09 -0300

    add arquivo terraform criando AKS

--- IaC terraform/criando-cluster-aks.tf
+++ IaC terraform/criando-cluster-aks.tf
@@ -3,11 +3,11 @@ resource "azurerm_resource_group" "app-java" {
   location = "westus"
 }
 
-resource "azurerm_kubernetes_cluster" "AKS" {
-  name                = "AKS-aks1"
-  location            = azurerm_resource_group.AKS.location
-  resource_group_name = azurerm_resource_group.AKS.name
-  dns_prefix          = "AKSaks1"
+resource "azurerm_kubernetes_cluster" "java" {
+  name                = "java-aks1"
+  location            = azurerm_resource_group.java.location
+  resource_group_name = azurerm_resource_group.java.name
+  dns_prefix          = "javaeaks1"
 
   default_node_pool {
     name       = "default"
@@ -15,22 +15,19 @@ resource "azurerm_kubernetes_cluster" "AKS" {
     vm_size    = "Standard_D2_v2"
   }
 
-  identity {
-    type = "SystemAssigned"
+  service_principal {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000000000000000000000000000"
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "example" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
 
   tags = {
     Environment = "Production"
   }
-}
-
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-
-  sensitive = true
 }
--- IaC terraform/criando-database.tf
+++ IaC terraform/criando-database.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "db" {
   location = "westus"
 }
 
-resource "azurerm_mssql_server" "db" {
+resource "azurerm_mssql_server" "db-java" {
   name                         = "mssqlserver"
-  resource_group_name          = azurerm_resource_group.db.name
-  location                     = azurerm_resource_group.db.location
+  resource_group_name          = azurerm_resource_group.db-java.name
+  location                     = azurerm_resource_group.db-java.location
   version                      = "12.0"
   administrator_login          = "java"
   administrator_login_password = "java@12345"
