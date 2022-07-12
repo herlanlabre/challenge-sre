aa6dc054182a6b15a24fd378648489712ecaa228
unknown <herlanroberto@gmail.com>
2022-07-12 15:46:12 -0300

    add arquivo provider.tf

--- IaC terraform/providers.tf
+++ IaC terraform/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = "3.13.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
