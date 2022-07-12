0c1bf13c010d1844c8da619a7b83ed445776f19c
unknown <herlanroberto@gmail.com>
2022-07-12 13:12:59 -0300

    add arquivo terraform criando cluster aks

---
+++ criando-cluster-aks.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "APP-JAVA" {
+  name     = "APP-JAVA-resources"
+  location = "Saopaulo/Brazil"
+}
+
+resource "azurerm_kubernetes_cluster" "K8S" {
+  name                = "K8S-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "K8S-ks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  service_principal {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000000000000000000000000000"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "example" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
